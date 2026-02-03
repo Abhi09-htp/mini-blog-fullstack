91c90b30a49f69601fdae12ffb63812e32bfece1
Abhishek Mane <abhishekashokmane09@gmail.com>
2026-02-03 13:29:38 +0530

    Add update and delete routes for blog posts

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import authMiddleware from "../middleware/auth.Middleware.js";
-import { createBlog, getBlogs } from "../controllers/blog.Controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
+import {
+  createBlog,
+  getBlogs,
+  updateBlog,
+  deleteBlog,
+} from "../controllers/blog.controller.js";
 
 const router = express.Router();
 
 router.get("/", getBlogs);
 router.post("/", authMiddleware, createBlog);
+router.put("/:id", authMiddleware, updateBlog);
+router.delete("/:id", authMiddleware, deleteBlog);
 
 export default router;
